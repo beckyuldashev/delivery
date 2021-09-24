0b05721ce77392402e912ac6e97a4b8e575ca3b0
Beck Yuldashev <yuldashev.bek@gmail.com>
2021-09-24 10:39:26 +0300

    Add change background header

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,3 +49,16 @@ function scrollActive() {
 }
 
 window.addEventListener('scroll', scrollActive);
+
+// ================ CHANGE BACKGROUND HEADER ===============
+function scrollHeader() {
+  const header = document.getElementById('header');
+
+  if (this.scrollY >= 80) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
